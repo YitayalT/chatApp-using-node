51737d12542bb20d1138efb5352e2194b235bc73
YitayalT <anex518@gmail.com>
2022-07-04 13:26:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+// app.all('', (req, res) => {
+//     res.sendFile('index');
+// })
+app.listen(5000, () => {
+    console.log('server is started at port 5000');
+});
