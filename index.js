bbc411a850a62a00aa2ca4176b89862136fb3ab7
YitayalT <anex518@gmail.com>
2022-07-05 05:05:32 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const Filter = require('bad-words');
 
 const socketio = require("socket.io");
 
@@ -19,9 +20,20 @@ io.on("connection", (socket) => {
   socket.emit('message', 'Welcome!');
   socket.broadcast.emit('message', 'A new user has added!');
 
-    socket.on("sendMessage", (message) => {
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback('Message is not allowed to be sent!');
+    }
+
       io.emit("message", message);
+      callback();
     });
+  
+  socket.on("sendLocation", (cords, callback) => {
+    io.emit("message", `https://google.com/maps?q=${cords.latitude},${cords.longitude}`);
+    callback(); 
+  });
   
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left!');
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,10 @@
 const socket = io();
 
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on('message', (message) => {
     console.log(message);
 });
@@ -7,10 +12,39 @@ socket.on('message', (message) => {
 document.querySelector("#message-form").addEventListener('submit',(e) => {
   e.preventDefault();
 
+  $messageFormButton.setAttribute('disabled', 'disabled');
   const message = document.querySelector("input").value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (err) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+    if (err) {
+     return console.log(err);
+    }
+    console.log("The message was delivered!");
+  });
   
+});
+
+$sendLocationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('geolocation is not supported by your browser!');
+  }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      $sendLocationButton.removeAttribute('disabled');
+      console.log('Location Shared!');
+    })
+  });
 });
 
 // socket.on("countUpdated", (count) => {
